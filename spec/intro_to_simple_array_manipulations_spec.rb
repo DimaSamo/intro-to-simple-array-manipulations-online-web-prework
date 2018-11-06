8f9a64b3d2e6564127cd732b8cfa35b446351918
Dmitri Samotessov <dimasamo96@gmail.com>
2018-11-06 03:07:08 +0000

    automatically backed up by learn

--- spec/intro_to_simple_array_manipulations_spec.rb
+++ spec/intro_to_simple_array_manipulations_spec.rb
@@ -87,7 +87,7 @@ end
 describe "#using_concat" do 
   before(:each) do 
     @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-    more_favs = ["mario kart", "flatiron school"]
+    @more_favs = ["mario kart", "flatiron school"]
     @all_my_favs = using_concat(@my_favorite_things, more_favs)
   end
    it "takes in two arguments of two different arrays and uses the concat method to add the contents of the second array to the first" do 
